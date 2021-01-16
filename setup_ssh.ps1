e55b72885fb889b4ce53ac917a3370217a3ffe07
Akar Singh <aks002@ucsd.edu>
2021-01-15 22:35:02 -0800

    General improvements to code readability - rid of aliases, uniform uppercase and argument listing, more comments, changed a few user prompts

--- setup_ssh.ps1
+++ setup_ssh.ps1
@@ -1,58 +1,74 @@
 # Begin by gathering credentials from user
 # ----------------------------------------
-echo "
+Write-Host "
 Enter some info first
 ------------------------------------------";
-$hostname=read-host("Hostname (IP)`t");
-$username=read-host("Username`t");
+$hostname=Read-host("Hostname (IP)`t");
+$username=Read-Host("Username`t");
 
+# Test the given host for a network connection, notifying user and terminating
+# script if failed to connect to host
 if (!(Test-Connection $hostname -Count 1 -Quiet)) {
-    write-host "`n!Hostname " -nonewline;
-    write-host "$hostname" -foregroundcolor Yellow -nonewline;
-    echo " was unreachable - terminating script...";
+    Write-Host -NoNewline "`n!Hostname ";
+    Write-Host -NoNewline -ForegroundColor Yellow "$hostname";
+    Write-Host " was unreachable - terminating script...";
     return;
 }
 
-# Path to current userprofile's .ssh folder to be used (beats typing it over
-# and over again)
+# Path to current userprofile's .ssh folder to be used
 $path="$env:userprofile\.ssh";
 
 # Create .ssh folder if not exist
-if (!(test-path "$path")){ 
-	mkdir $path;
+if (!(Test-Path "$path")){ 
+	New-Item $path;
 }
 
-# List all existing keys and ask user to input they key pair they wish to use
-# Else, if no matches, new key will be created
-
-echo "`nLooking for key pairs in the '$path' directory...";
+# Look through ~/.ssh for existing key/key.pub pairs and list them to user
+Write-Host "`nLooking for key pairs in the '$path' directory...";
 foreach($pubkey in (ls $path\*.pub).Name){
     $key = $pubkey.Replace(".pub", "");
-    if (test-path $path\$key){
-        write-host -NoNewline "`to Key pair ";
-        write-host -NoNewline -ForegroundColor Yellow "$key";
-        Write-Host " was found";}
-}
-if (!($key)){
-    write-host -ForegroundColor Yellow "`no No Key pairs found!!";
+    if (Test-Path $path\$key){
+        Write-Host -NoNewline "`to Key pair ";
+        Write-Host -NoNewline -ForegroundColor Yellow "$key";
+        Write-Host " was found";
+    }
 }
 
-$key=read-host("`nSelect Key Pair`t");
+# List all existing keys and prompt user to input they key pair they wish to use
+# Else, if no matches, prompt user for new key pair name
 if (!($key)){
-	echo "No input provided - defaulting to id_rsa";
+    Write-Host -ForegroundColor Yellow "`no No Key pairs found!!";
+    $key=Read-Host("`nName for new Key Pair`t");
+}
+else{
+    $key=Read-Host("`nSelect Key Pair`t");
+}
+
+# Default to id_rsa if no input provided
+if (!($key)){
+	Write-Host "No input provided - defaulting to id_rsa";
 	$key="id_rsa";
 }
 
-if ((test-path $path\$key) -And (test-path "$path\$key.pub")){
-    write-host "`nUsing existing key pair " -NoNewline;
-    write-host $key -ForegroundColor Yellow;
+# Notify user of whether new key will be generated (user input nonexisting) 
+# or selected key will be used (use input existing)
+if ((Test-Path $path\$key) -And (Test-Path "$path\$key.pub")){
+    Write-Host -NoNewline "`nUsing existing key pair ";
+    Write-Host -ForegroundColor Yellow $key;
 }
-else {
-    write-host "`nGenerating new key pair " -NoNewline;
-    write-host $key -ForegroundColor Yellow;
 
-    $comment=read-host("`nComment to append to new key [e.g. device name or email]");
-    write-host
+# If input key unrecognized (regardless of pre-existing keys), a new key with
+# given name will be generated
+else {
+    Write-Host -NoNewline "`nGenerating new key pair ";
+    Write-Host -ForegroundColor Yellow $key;
+
+# Key created using rsa algorithm with 4096 Bytes and no passphrase
+# Prompt user for addition comment info to generate new key with and use 
+# appropriate arguments based on whether comment was given or not
+    $comment=Read-Host("`nComment to append to new key `n [Usually name of
+            device you're connecting with or user email]`nComment`t");
+    Write-Host();
 
     if ($comment){
         ssh-keygen -b 4096 -t rsa -C $comment -N '""' -f "$path\$key";
@@ -61,31 +77,36 @@ else {
         ssh-keygen -b 4096 -t rsa -N '""' -f "$path\$key";
     }
 
-    write-host "`nSucessfully created key pair " -NoNewline;
-    write-host $key -ForegroundColor Yellow;
+# Notify upon successful key creation
+    Write-Host -NoNewline "`nSucessfully created key pair ";
+    Write-Host -ForegroundColor Yellow $key;
 }
 
-echo "`nNow SSHing into $username@$hostname to add public key to list of authorized keys";
-echo "This should be the last time you'll have to manually enter a password!`n";
+# Announce the need to enter a password and the reason (appending public key)
+Write-Host "`nNow SSHing into $username@$hostname to add public key to list of authorized keys";
+Write-Host "This should be the last time you'll have to manually enter a password!`n";
 
 # SSH into host to add user public key to 'authorized_keys' in host
-cat "$path\$key.pub" | 
+Get-Content "$path\$key.pub" | 
 ssh $username@$hostname "cat>temp && mkdir -p ~/.ssh && cat temp>>~/.ssh/authorized_keys && rm temp";
 
+# If ssh connection failed (wrong password or non-response), notify user of
+# detected failure and exit script
 if($LASTEXITCODE){
-    echo "`nFailed to connet with SSH - terminating script...";
+    Write-Host "`nFailed to connect with SSH - terminating script...";
     return;
 }
 
-# Ask for shortcut name for logging into host
-echo "`nPick a nickname for $hostname you'd prefer to use"
-echo "[generally a short name to call when running ssh <nickname>]";
-$nickname=read-host("Nickname`t")
+# Ask for shortcut name for logging into host if key succesfully appended
+Write-Host "`nPick a nickname for $hostname you'd prefer to use"
+Write-Host "[Generally a short name, run like this --> ssh <nickname>]";
+$nickname=Read-Host("Nickname`t")
 
 # Add configurations for host onto user's ssh config file
-echo "Host $nickname`n`tHostname $hostname`n`tUser $username`n`tIdentityFile `"$path\$key`"" |
+# Specify ASCII encoding for Powershell V5.1 (defaults to utf8)
+Write-Output "Host $nickname`n`tHostname $hostname`n`tUser $username`n`tIdentityFile `"$path\$key`"" |
 Out-File -Append -Encoding ASCII "$path\config"
 
-# Notify user of completion
-write-host -NoNewline "Setup complete, you can log into $hostname as $username by just calling ";
-write-host -NoNewline -ForegroundColor Yellow "ssh $nickname"; write-host " now";
+# Notify user of script completion and how to use the new shortcut
+Write-Host -NoNewline "Setup complete, you can log into $hostname as $username by just calling ";
+Write-Host -NoNewline -ForegroundColor Yellow "ssh $nickname"; Write-Host " now";
